bada6087cdf363577270e7c5099020fe05849871
Arthur Puyou <arthur@puyou.fr>
2013-03-12 22:36:27 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Faux CAS
+========
+
+Ceci est un faux CAS pour pouvoir tester une application utilisant un CAS sans internet.
+
+Il n'y a aucun contrôle de mot de passe et une vérification de ticket plus que minimale.
+
+Copiez ce dossier dans votre `/var/www` et configurez son URL dans votre application.
+
+D'après une idée originale de [Florent Thévenet](https://github.com/feuloren) pour [Le Polar](http://assos.utc.fr/polar).
---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ *  Copyright (C) 2013 payutc <payutc@assos.utc.fr>
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+session_start();
+
+function gen_ticket_and_redirect($url) {
+  $ticket = $_SESSION['login'] . '@' . $url . '@' .rand(1, 1000);
+  $_SESSION[$url]['ticket'] = $ticket;
+  header('Location: ' . $url . '?ticket=' . $ticket);
+}
+
+switch ($_GET['action']) {
+case 'login':
+  $service = $_GET['service'];
+  if (isset($_SESSION['state']) AND $_SESSION['state'] === 'ok') {
+    gen_ticket_and_redirect($service);
+  } else {
+    include 'form.html';
+  }
+  break;
+case 'auth':
+  $service = $_GET['service'];
+  $_SESSION['login'] = $_GET['login'];
+  gen_ticket_and_redirect($service);
+  break;
+case 'serviceValidate':
+  $service = $_GET['service'];
+  $ticket = $_GET['ticket'];
+  $tab = explode("@", $ticket);
+  if ($service == $tab[1]) {
+    $user = $tab[0];
+    $_SESSION['state'] = 'ok';
+    include 'success.xml';
+  } else {
+    include 'fail.xml';
+  }
+  break;
+case 'logout':
+  unset($_SESSION['state']);
+  unset($_SESSION['login']);
+  $url = $_GET['url'];
+  header("Location: $url");
+  break;
+default:
+  die('Not a valid action !');
+}
+
+?>
